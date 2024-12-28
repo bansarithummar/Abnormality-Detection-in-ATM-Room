da55b5b70508ec85ef92ca33a129bba514d668b2
Bansari Thummar <bansarithummar@gmail.com>
2024-12-27 20:10:46 -0600

    Update ViolaJones_new.m

--- ViolaJones_new.m
+++ ViolaJones_new.m
@@ -3,5 +3,5 @@ function faceCount = ViolaJones(sampleFrame)
 %To detect Face
 FDetect = vision.CascadeObjectDetector;
 
-%Returns Bounding Box values based on number of objects
+%Returns Bounding Box values based on the number of objects
 faceCount = step(FDetect,sampleFrame);
