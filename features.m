4e071bae6fa8f09037897415188407218674a1cc
Bansari Thummar <bansarithummar@gmail.com>
2025-02-27 08:57:30 -0600

    Update features.m

--- features.m
+++ features.m
@@ -1,10 +1,12 @@
 function [fpos, fneg] = features(pathPos,pathNeg)
+    
     % extract features for positive examples
     imlist = dir([pathPos '*.png']);
     for i = 1:length(imlist)
         im = imread([pathPos imlist(i).name]);
         fpos{i} = HOG(double(im));
     end
+    
     % extract features for negative examples
     imlist = dir([pathNeg '*.png']);
     for i = 1:length(imlist)
