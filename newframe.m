156a104dc258b670f2f09ee5a0e4f681e26cd805
Bansari Thummar <bansarithummar@gmail.com>
2025-03-01 12:18:02 -0600

    Update newframe.m

--- newframe.m
+++ newframe.m
@@ -3,8 +3,8 @@ close all;
 
 % Open an sample avi file
 
-%[FileName,PathName] = uigetfile('*.mp4','D:\video');
-file = fullfile('D:\Database\video5.mp4');
+%[FileName,PathName] = uigetfile('*.mp4','*\video');
+file = fullfile('*\*****\video5.mp4');
 
 %filename = '.\003.AVI';
 mov = VideoReader(file);
@@ -19,19 +19,23 @@ end
 %getting no of frames
 numberOfFrames = mov.NumberOfFrames;
 numberOfFramesWritten = 0;
-for frame = 225 : 235
 
+for frame = 225 : 235
     thisFrame = read(mov, frame);
     thisFrame = imrotate(thisFrame,-90);
     outputBaseFileName = sprintf('%3.3d.png', frame);
     outputFullFileName = fullfile(outputFolder, outputBaseFileName);
+    
     imwrite(thisFrame, outputFullFileName, 'png');
 	faceCount = ViolaJones(thisFrame);
+    
     progressIndication = sprintf('Wrote frame %4d of %d.', frame,numberOfFrames);
 	detectedFaces = sprintf('Number of faces in frame %4d : %d', frame,faceCount);
+    
     disp(progressIndication);
 	disp(detectedFaces);
     numberOfFramesWritten = numberOfFramesWritten + 1;
 end
+
 progressIndication = sprintf('Wrote %d frames to folder "%s"',numberOfFramesWritten,outputFolder);
 disp(progressIndication);
