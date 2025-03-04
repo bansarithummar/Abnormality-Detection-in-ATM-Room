f7412b4044eb5558e0e9eb8625a98ee920bb998d
Bansari Thummar <bansarithummar@gmail.com>
2025-03-04 08:45:32 -0600

    Update violaget.m

--- violaget.m
+++ violaget.m
@@ -1,12 +1,11 @@
 clear all
 clc
-%Detect objects using Viola-Jones Algorithm
 
 %To detect Face
 FDetect = vision.CascadeObjectDetector;
 
 %Read the input image
-I = imread('D:\505.png');
+I = imread('*:\505.png');
 
 %Returns Bounding Box values based on number of objects
 BB = step(FDetect,I);
@@ -16,5 +15,6 @@ imshow(I); hold on
 for i = 1:size(BB,1)
     rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
 end
+
 title('Face Detection');
 hold off;
