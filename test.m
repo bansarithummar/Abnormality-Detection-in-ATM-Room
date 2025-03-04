5789dfa5734b73e8e55cddd54690b9f3fb51b53b
Bansari Thummar <bansarithummar@gmail.com>
2025-03-03 20:58:20 -0600

    Update test.m

--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 % This is for path
 
-pathFull = './****/clear face/'; % clear face
-pathPartial = './****/Partial Face/'; % partial face
+pathFull = './****/clear face/'; 
+pathPartial = './****/Partial Face/'; 
 
 [fullFace, PartFace] = LBPfeatures(pathFull, pathPartial); 
 new = cat(2,fullFace, PartFace);
