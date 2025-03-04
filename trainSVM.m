2cf0ba7682de573e8c7af2a48ed10999605be414
Bansari Thummar <bansarithummar@gmail.com>
2025-03-04 08:45:09 -0600

    Update trainSVM.m

--- trainSVM.m
+++ trainSVM.m
@@ -1,9 +1,11 @@
 function [ model ] = trainSVM( fpos,fneg )
-    SV = loadingV(fpos,fneg);   % loading and labeling each training example
+    SV = loadingV(fpos,fneg);   
     fprintf('Training SVM..\n');
+    
     T = cell2mat(SV(1,:));
     P = cell2mat(SV(2,:)'); % each row of P correspond to a training example 
     disp(SV);
+    
     model = svmlearn(P, T', '-t -g 0.3 -c 0.5');
     fprintf('done. \n');
 end
