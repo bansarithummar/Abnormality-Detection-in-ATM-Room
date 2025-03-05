5c956d9ad5102417399c816799c812e325ed996f
Bansari Thummar <bansarithummar@gmail.com>
2025-03-05 16:30:12 -0600

    Update LBPfeatures.m

--- LBPfeatures.m
+++ LBPfeatures.m
@@ -1,19 +1,21 @@
 function [fullFace, PartFace] = LBPfeatures(pathFull, pathPartial)   
     imlist = dir([pathFull '*.png']);
     fullFace = zeros(length(imlist),59);
+    
     for i = 1:length(imlist)
-        
         im = imread([pathFull imlist(i).name]);
         im = imresize(im,[25 32]);
         if size(im,3)==3
             img=rgb2gray(im);
         end
+        
         fullFace(i,:) = extractLBPFeatures(img);
     end
     
     % extract features for negative examples
     imlist = dir([pathPartial '*.png']);
     PartFace = zeros(length(imlist),59);
+    
     for i = 1:length(imlist)
         im = imread([pathPartial imlist(i).name]);
         im = imresize(im,[25 32]);
